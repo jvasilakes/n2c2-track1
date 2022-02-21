85869d376c057e3bfc5152bf87ffabf95c759f80
Jake Vasilakes <jvasilakes@gmail.com>
2022-02-21 14:04:29 +0000

    Output per-class results to log file

--- context/eval_scripts/eval_context.py
+++ context/eval_scripts/eval_context.py
@@ -51,18 +51,41 @@ def main(args):
     for task in all_preds_by_task.keys():
         preds = all_preds_by_task[task]
         golds = all_golds_by_task[task]
+        labels = sorted(set(golds))
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
             micro_p, micro_r, micro_f, _ = precision_recall_fscore_support(
                     golds, preds, average="micro")
             macro_p, macro_r, macro_f, _ = precision_recall_fscore_support(
                     golds, preds, average="macro")
+            lab_p, lab_r, lab_f, lab_s = precision_recall_fscore_support(
+                    golds, preds, average=None, labels=labels)
+            per_label_table = format_per_label_results(
+                    lab_p, lab_r, lab_f, lab_s, labels=labels)
         print(f"### {task}                     ")
         print(f"|      | {'prec': <4}  | {'rec': <3}   | {'f1': <4}  |")
         print("|------|-------|-------|-------|")
         print(f"|MICRO | {micro_p:.3f} | {micro_r:.3f} | {micro_f:.3f} |")
         print(f"|MACRO | {macro_p:.3f} | {macro_r:.3f} | {macro_f:.3f} |")
         print()
+
+        with open(args.log_file, 'a') as outF:
+            outF.write(f"### {task}\n")
+            outF.write(per_label_table)
+
+
+def format_per_label_results(precs, recs, fs, supports, labels=[]):
+    if labels == [] or len(labels) != len(precs):
+        labels = [str(i) for i in range(len(precs))]
+
+    max_chars = max([len(lab) for lab in labels]) + 1
+    tab = f"|{' ': <{max_chars}}| {'prec': <4}  | {'rec': <3}   | {'f1': <4}  | {'supp': <4} |\n"  # noqa
+    tab += f"|{'-' * max_chars}|-------|-------|-------|------|\n"
+
+    for (l, p, r, f, s) in zip(labels, precs, recs, fs, supports):
+        tab += f"|{l: <{max_chars}}| {p:.3f} | {r:.3f} | {f:.3f} | {s: <4} |\n"
+    tab += '\n'
+    return tab
 
 
 if __name__ == "__main__":
