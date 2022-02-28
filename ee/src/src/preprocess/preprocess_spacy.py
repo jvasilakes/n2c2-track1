4b3c62e200204c90c0eec6805ceea8fc97a24c5d
panagiotis-geo <panagiotis.georgiads@gmail.com>
2022-02-28 15:31:36 +0000

    Updated configs and src code

---
+++ ee/src/src/preprocess/preprocess_spacy.py
@@ -0,0 +1,295 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 03-Feb-2022
+
+author: panos
+"""
+
+import os
+import sys
+import re
+import numpy as np
+from os import listdir
+from os.path import isfile, join
+from tqdm import tqdm
+import json
+from collections import OrderedDict
+
+
+genia_splitter = os.path.join("../common", "geniass")
+temp_dict = {'T':'entities','E':'events', 'Certainty':'certainty', 'Actor': 'actor', 
+             'Action':'action', 'Temporality':'temporality', 'Negation':'negation'}
+###############################
+spacy_ign = ["_","#","\n",'"',"@",'-']
+
+def spacy_replace(s):
+    for special in spacy_ign:
+        s = s.replace(special, '')
+    return s
+def parse_annotation_line(line):
+    x = line.strip().split('\t')
+    if x[0][0]=='T': # ( tr_id  (cl st end)  name)
+        if len(x[1].split(';')) > 1: # ( tr_id  (cl st end; st end)  name)
+            cl, st = x[1].split(' ')[:2]
+            end =  x[1].split(' ')[-1]
+        else:
+            cl, st, end = x[1].split(' ')
+        
+        annot = {'id': x[0], 'name': '\t'.join(x[2:]),'st':int(st), 'end':int(end), 'cl':cl} #sent_no word_id bio
+        return temp_dict['T'], annot
+    
+    elif x[0][0]=='E': # ( ev_id (cl:tr_id) )
+        eid = x[0]
+        cl, tr = x[1].split(':')
+        annot = {'eid': eid, 'event-type':cl, 'tr':tr}
+        return temp_dict['E'], annot
+    elif x[0][0]=='A': # ( a_id type ev_id cat)
+#         typ = x[1].split(' ')[0]
+        typ, eid, cl = x[1].split(' ')
+        annot = {'aid':x[0], 'type':typ, 'eid': eid,'cl':cl}
+        return temp_dict[typ], annot
+    else:
+        print(x)
+        raise Exception
+        
+
+def get_offsets(sentences):
+    offsets = {0:  0}
+    for i in range(len(sentences)-1):
+        offsets[i+1] = offsets[i] + len(sentences[i]) + 1
+    return offsets
+
+def adjust_offsets(old_sents, new_sents, old_entities, f):
+    """
+    Adjust offsets based on tokenization
+    Args:
+        old_sents: (list) old, non-tokenized sentences
+        new_sents: (list) new, tokenized sentences
+        old_entities: (dic) entities with old offsets
+    Returns:
+        new_entities: (dic) entities with adjusted offsets
+        abst_seq: (list) abstract sequence with entity tags
+    """
+    original = " ".join(old_sents)
+    newtext = " ".join(new_sents)
+    new_entities = []
+    terms = {}
+    for e in old_entities:
+        start = e['st']
+        end = e['end']
+        old_pos = str(start)+' '+str(end)
+        if (start, end) not in terms:
+            terms[(start, end)] = [[start, end,  e['cl'], e['name'], e['id'], old_pos]]
+        else:
+            terms[(start, end)].append([start, end, e['cl'], e['name'], e['id'], old_pos])
+
+    orgidx, newidx = 0, 0
+    orglen, newlen = len(original), len(newtext)
+    terms2 = terms.copy()
+    while orgidx < orglen and newidx < newlen:
+        # print(repr(original[orgidx]), orgidx, repr(newtext[newidx]), newidx)
+        if original[orgidx] == newtext[newidx]:
+            orgidx += 1
+            newidx += 1
+        elif original[orgidx] == "`" and newtext[newidx] == "'":
+            orgidx += 1
+            newidx += 1
+        elif newtext[newidx] == '\n':
+            newidx += 1
+        elif original[orgidx] == '\n':
+            orgidx += 1
+        elif newtext[newidx] == ' ':
+            newidx += 1
+        elif original[orgidx] == ' ':
+            orgidx += 1
+        elif newtext[newidx] == '\t':
+            newidx += 1
+        elif original[orgidx] == '\t':
+            orgidx += 1
+        elif original[orgidx] in spacy_ign: 
+            orgidx += 1
+        elif newtext[newidx] in spacy_ign:
+            newidx += 1
+
+        else:
+            print("Non-existent text in file %s: %d\t --> %s != %s " % (f, orgidx, repr(original[orgidx-10:orgidx+10]),
+                                                             repr(newtext[newidx-10:newidx+10])))
+            print('newtext', newtext)
+            exit(0)
+            
+
+        starts = [key[0] for key in terms2.keys()]
+        ends = [key[1] for key in terms2.keys()]
+
+        if orgidx in starts:
+            tt = [key for key in terms2.keys() if key[0] == orgidx]
+            for sel in tt:
+                for l in terms[sel]:
+                    l[0] = newidx
+
+        if orgidx in ends:
+            tt2 = [key for key in terms2.keys() if key[1] == orgidx]
+            for sel2 in tt2:
+                for l in terms[sel2]:
+                    if l[1] == orgidx:
+                        l[1] = newidx
+
+            for t_ in tt2:
+                del terms2[t_]
+        
+    #   fix_sent_break
+    newtext_break = "\n".join(new_sents)
+    # check if test has issue with splittig entity
+    sents_break = newtext_break.split('\n')
+    ##
+    cur = 0
+    new_sent_range = []
+    for s in sents_break:
+        new_sent_range += [(cur, cur + len(s))]
+        cur += len(s) + 1
+        
+    ent_sequences = []
+    for ts in terms.values():
+        for term in ts:
+            new_ts = newtext_break[term[0]:term[1]].replace(" ", "").replace("\n", "")
+            new_ts = spacy_replace(new_ts)
+            old_ts = term[3].replace(" ", "").replace('\n', '')
+            old_ts = spacy_replace(old_ts)
+            if new_ts != old_ts:
+                if new_ts.lower() == old_ts.lower():
+                    tqdm.write('ENT_ID {}, Lowercase Issue: {} <-> {}'.format(term[4],new_ts, old_ts))
+                    
+                else:
+                    tqdm.write('ENT_ID {}, Entities do not match: {} <-> {}'.format(term[4], new_ts, old_ts))
+                exit()
+                    
+            # Find sentence number of each entity
+            sent_no = []
+            for s_no, sr in enumerate(new_sent_range):
+                if set(np.arange(term[0], term[1])).issubset(set(np.arange(sr[0], sr[1]))):
+                    sent_no += [s_no]
+
+            assert (len(sent_no) == 1), '{} ({}, {}) -- {} -- {} <> {}'.format(sent_no, term[0], term[1],
+                                                                               new_sent_range,
+                                                                               newtext_break[term[0]:term[1]], term[3])
+            new_entity = {'id':term[4],'name': newtext[term[0]:term[1]],'st':int(term[0]), 
+                          'end':int(term[1]), 'cl':term[2], 'sent_no': sent_no[0], 'old_pos': term[5]}
+
+            new_entities.append(new_entity)
+
+
+    return new_entities, new_sents
+
+### Convert dataset to Pubtator
+filt = ["LICENSE","README"]
+
+def check_overlap(ord_dict,f):
+    prev_en = -1
+    for i, (st,en) in enumerate(ord_dict):
+        if st < prev_en:
+            print("We have overlapping issue here",f)
+            return True
+        prev_en = en
+    return False
+
+def preprocess_spacy(data_path,folder_path, window):
+    onlyfiles = [f for f in listdir(data_path) if isfile(join(data_path, f)) and f not in filt]
+    uniquefiles = list(set([f.split(".")[0] for f in onlyfiles if f.split(".")[0]]))
+    uniquefiles.sort()
+
+    ### Lets work with test 
+    stats = {'entity_count':0, 'event_count': 0, 'event_type_count' :{'Disposition':0,'NoDisposition':0,'Undetermined':0},'unique_entity_count':0, 'no_sents':0, 'len_sents':0, 'avg_sent':0}
+    with open(join(folder_path,data_path.split('/')[-2]+'_data.txt'),"w") as fout:
+        with open(join(folder_path,data_path.split('/')[-2]+'_spacy_special.txt'),"w") as fspecial:
+            for f in tqdm(uniquefiles[0:]):
+                data = {'entities':[], 'events':[], 'certainty':[], 'actor':[],
+                        'action':[], 'temporality':[], 'negation':[]}
+                with open(join(data_path,f+".txt"),'r') as infile:
+                    lines = infile.readlines()
+                
+                with open(join(data_path,f+'.ann'), 'r') as annfile:
+                    for line in annfile.readlines():
+                        cat, parsed = parse_annotation_line(line)
+                        data[cat].append(parsed)
+                        
+                with open(join(folder_path+data_path.split('/')[-2], f+"_spacy.json"), 'r') as senfile:
+                    new_sentences = [json.loads(line.strip()) for line in senfile]
+                ## Processing
+                orig_sentences = ''.join(lines).split('\n')
+                split_sentences = [sent['_text'] for sent in new_sentences]
+                new_entities, split_sentences = adjust_offsets(orig_sentences, split_sentences, data['entities'], f)
+                data['entities'], data['sents']  = new_entities, split_sentences
+                stats['no_sents'] += len(split_sentences)
+                stats['len_sents'] += sum([len(s) for s in split_sentences])
+                stats['entity_count'] += len(data["entities"])
+                stats['event_count'] += len(data["events"])
+                ###
+                for ev in data['events']:
+                    stats['event_type_count'][ev['event-type']] +=1
+                unique = {}
+                for ent in data['entities']:
+                    if (ent['st'],ent['end']) in unique:
+                        unique[(ent['st'],ent['end'])].append(ent)
+                    else:
+                        unique[(ent['st'],ent['end'])] = [ent]
+                stats['unique_entity_count'] += len(unique.keys())
+                overlap = check_overlap(OrderedDict(sorted(unique.items())),f)
+                if overlap:
+                    exit
+                ###
+                max_sent = len(split_sentences)
+                offsets = get_offsets(split_sentences)
+                ## Adding verb information
+                sent_verbs = {}
+                for sent in new_sentences:
+                    sent_verbs[sent["sent_index"]] = sent["verbs"]
+                for st,end in unique.keys(): # Each is gonna be a sample
+                    entries = unique[(st,end)]
+                    sent_no = entries[0]['sent_no']
+                    sent_range = np.arange(start=max(0,sent_no - window), stop=min(max_sent,sent_no + window + 1))
+                    sample = {'text': ' '.join(list(split_sentences[i] for i in sent_range))}
+                    offset = offsets[min(sent_range)]
+                    sample['trig'] = {'s': st - offset, 'e':end -offset, 'name': entries[0]['name'], 'old_pos': entries[0]['old_pos']}
+                    if sample['text'][sample['trig']['s']:sample['trig']['e']]!= entries[0]['name']:
+                        print('Problem {}  <> {}'.format(sample['text'][sample['trig']['s']:sample['trig']['e']], entries[0]['name']))
+                    ids = [entry['id'] for entry in entries]
+                    sample['events'] = [event['event-type'] for event in data['events'] if event['tr'] in ids]
+                    sample['fname'] = f
+                    ###
+                    eids = [event['eid'] for event in data['events'] if event['tr'] in ids]
+                    sample['actions'] =[action['cl'] for action in data['action'] if action['eid'] in eids]
+                    if len(sample['actions'])> 1:
+                        print('We got you ', f, eids, sample['actions'])
+                    ###
+                    sample['verbs'] = []
+                    curr = 0
+                    for sent_no in sent_range:
+                        verbs_sent = sent_verbs[sent_no]
+                        for verb in verbs_sent:
+                            sample['verbs'].append({"t":verb["t"], "st":verb["st"]+curr, "en":verb["en"]+curr})
+                        curr += len(split_sentences[sent_no]) + 1
+                    for verb in sample['verbs']: #check
+                        if sample['text'][verb['st']:verb['en']]!= verb['t']:
+                            print('Problem {}  <> {}'.format(sample['text'][verb['st']:verb['en']], verb['t']))
+                    fout.write(json.dumps(sample) + '\n')
+                    if len(entries) > 1 or len(sample['events']) > 1:
+                        fspecial.write(json.dumps(sample) + '\n')
+            stats['avg_sent'] = stats['len_sents'] /stats['no_sents']
+    return stats
+
+
+def main():
+    # from preprocess import preprocess
+    data_path = ["../data/train/","../data/dev/"] 
+    preprocess_path = "../data/preprocessed/"
+
+    stats = {}
+    for i,path in enumerate(data_path):
+        stats[path.split('/')[-2]] = preprocess(path, preprocess_path, 1)
+
+    with open(join(preprocess_path,'stats.txt'),"w") as st_out:
+         st_out.write(json.dumps(stats))
+        
+if __name__ == "__main__":
+    main()
---
+++ ee/src/src/trainer.py
@@ -0,0 +1,233 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 03-Mar-2020
+
+author: fenia
+"""
+
+import torch
+from torch import nn, optim
+import torch.nn.functional as F
+from blank_net import BlankNet as target_model
+from helpers.base_trainer import BaseTrainer
+from helpers.io import *
+from time import time
+import numpy as np
+import json
+from sklearn.metrics import f1_score, precision_recall_fscore_support, classification_report
+from torch.cuda.amp import autocast, GradScaler
+#
+
+class Trainer(BaseTrainer):
+    def __init__(self, config, device, iterators=None, vocabs=None):
+        """
+        Trainer object.
+        Args:
+            config (dict): model parameters
+            iterators (dict): 'train' and 'test' iterators
+        """
+        self.config = config
+        self.iterators = iterators
+        self.vocabs = vocabs #they are inversed
+        self.device = device
+
+        self.model = None  # self.init_model(target_model)
+        self.optimizer = None   # self.set_optimizer(self.model, opt=self.config['optimizer'])
+        self.scheduler = None
+        self.primary_metric = []  # on the validation set
+        self.best_score = 0
+        self.best_epoch = 0
+        self.cur_patience = 0
+        self.averaged_params = {}
+        self.iterations = len(self.iterators['train'])
+        self.save_path = os.path.join(self.config['model_folder'], 'bert.model')
+        self.scaler = GradScaler()
+
+#     def optimise(self):
+#         """
+#         what is the purpose
+#         """
+#         print('Why I am inside optimise?')
+#         exit()
+#         return 
+
+        
+    def calculate_stats(self, y_true, y_sigmoid, typ, keyword=''):
+        y_pred =  np.array(y_sigmoid > self.config['threshold'], dtype=float)
+        if typ =='actions':
+            print('\t{} eval: y_pred size {} y_pred sum {}'.format(keyword, y_pred.shape, np.sum(y_pred)))
+#             print(typ, 'y_pred size',y_pred.shape, 'y_pred sum', np.sum(y_pred))
+        mi_f1 = f1_score(y_true, y_pred, average='micro')
+#         res = classification_report(y_true, y_pred, output_dict=True, zero_division=1)
+#         perf = {self.vocab[typ]:res[str(i)]['f1-score'], for i in range(0, len(res))}
+        pr, re, f1, _ = precision_recall_fscore_support(y_true, y_pred, average='macro', zero_division=1)
+        return {'micro_f1':mi_f1, 'macro_pr':pr , 'macro_re':re, 'macro_f1':f1,'pred':y_pred}
+        
+    def calculate_performance(self, epoch, tracker, time_, mode='train'):
+        tracker['total'] = np.mean(tracker['total'])
+        perf = {}
+#         for typ in ['events', 'actions']:
+#             y_true = np.vstack([entry[1] for entry in tracker[typ]]) 
+#             y_sigmoid = np.vstack([entry[0] for entry in tracker[typ]])
+#             perf[typ] = self.calculate_stats(y_true, y_sigmoid, typ)
+#             print_performance(epoch, tracker, typ, perf[typ], time_, name=mode)
+        ## Events
+        y_true_ev = np.vstack([entry[1] for entry in tracker['events']]) 
+        y_sigmoid_ev = np.vstack([entry[0] for entry in tracker['events']])
+        perf['events'] = self.calculate_stats(y_true_ev, y_sigmoid_ev, 'events')
+        true_dispo = y_true_ev[:,1] == 1 
+        pred_dispo = y_sigmoid_ev[:,1] > self.config['threshold']
+        fail_dispo =  np.logical_and(y_true_ev[:,1] == 1,y_sigmoid_ev[:,1] < self.config['threshold'])
+        total_dispo = np.logical_or(true_dispo, pred_dispo)
+        disp_count=(np.sum(true_dispo),np.sum(pred_dispo),np.sum(total_dispo),len(true_dispo))
+        print_performance(epoch, tracker, 'events', perf['events'], time_, mode, disp_count)
+        ## Pred + True
+        y_true_ac = np.vstack([entry[1] for entry in tracker['actions']])[total_dispo] 
+        y_sigmoid_ac = np.vstack([entry[0] for entry in tracker['actions']])[total_dispo]
+        perf['actions'] = self.calculate_stats(y_true_ac, y_sigmoid_ac, 'actions', 'Pred+True')
+        print_performance(epoch, tracker, 'actions', perf['actions'], time_, mode)
+        ## All events
+        y_true_ac = np.vstack([entry[1] for entry in tracker['actions']]) 
+        y_sigmoid_ac = np.vstack([entry[0] for entry in tracker['actions']])
+        perf['actions_all'] = self.calculate_stats(y_true_ac, y_sigmoid_ac, 'actions','All')
+        print_performance(epoch, tracker, 'actions', perf['actions_all'], time_, mode)
+        # Pred Disposition
+        y_true_ac = np.vstack([entry[1] for entry in tracker['actions']])[total_dispo] 
+        y_sigmoid_ac = np.vstack([entry[0] for entry in tracker['actions']])
+        y_sigmoid_ac[fail_dispo] -= 1
+        y_sigmoid_ac = y_sigmoid_ac[total_dispo] 
+        perf['actions_script'] = self.calculate_stats(y_true_ac, y_sigmoid_ac, 'actions','Script')
+        print_performance(epoch, tracker, 'actions', perf['actions_script'], time_, mode)
+        ## True disposition
+        y_true_ac = np.vstack([entry[1] for entry in tracker['actions']])[true_dispo] 
+        y_sigmoid_ac = np.vstack([entry[0] for entry in tracker['actions']])[true_dispo]
+        perf['actions_true'] = self.calculate_stats(y_true_ac, y_sigmoid_ac, 'actions', 'True')
+        print_performance(epoch, tracker, 'actions', perf['actions_true'], time_, mode)
+        ##
+        tracker['event_preds'] = perf['events']['pred']
+        tracker['action_preds'] = perf['actions_all']['pred']
+#         if mode!='train':
+#             wrong = np.sum(y_true,1)!=np.sum(y_pred,1)
+#             samples = np.concatenate(tracker['samples'])[wrong]
+#             tracker['samples'] = samples
+#             tracker['preds'] = y_pred[wrong]
+# #             print('Wrong samples', samples)
+        return perf['events']
+
+
+    @staticmethod
+    def init_tracker():
+        return {'total': [], 'events': [], 'actions': [], 'total_samples': 0, 'samples': [], 'event_preds': [], 'action_preds':[]}
+
+    def run(self):
+        """
+        Main Training Loop.
+        """
+#         self.print_params2update(self.model)
+        print_options(self.config)
+        self._print_start_training()
+        
+        for epoch in range(1, self.config['epochs'] + 1):
+            train_tracker, time_ = self.train_epoch(epoch)
+
+            _ = self.calculate_performance(epoch, train_tracker, time_, mode='train')
+            
+            dev_tracker, time_ = self.eval_epoch(iter_name='dev')
+            dev_perf = self.calculate_performance(epoch, dev_tracker, time_, mode='dev')
+            self.primary_metric += [dev_perf['micro_f1']]
+   
+            stop = self.epoch_checking_larger(epoch, self.primary_metric[-1])
+            print('current best epoch:', self.best_epoch)
+            ##
+            if self.best_epoch == epoch:
+                print_preds(dev_tracker, self.iterators['dev'], self.config, epoch, 'dev')
+#                 print_cases(dev_tracker['samples'], dev_tracker['preds'], self.iterators['dev'], self.config, epoch)
+            ##
+            if stop:
+                break
+            print()
+
+        self._print_end_training()
+
+        print('Best epoch: {}\n'.format(self.best_epoch))
+        return self.best_score
+
+    def train_epoch(self, epoch):
+        """
+        Evaluate the model on the train set.
+        """
+        t1 = time()
+        self.model = self.model.train()
+        tracker = self.init_tracker()
+
+        iterations = len(self.iterators['train'])
+        for batch_idx, batch in enumerate(self.iterators['train']):
+            step = ((epoch-1) * iterations) + batch_idx
+            tracker['total_samples'] += len(batch['names'])
+            
+            for keys in batch.keys():
+                if keys not in ['names', 'old_pos']:
+                    batch[keys] = batch[keys].to(self.device)
+
+            with autocast():
+                eloss, aloss, esigmoid, asigmoid = self.model(batch)  # forward pass
+                loss = self.config['event_weight'] * eloss + \
+                           (1 - self.config['event_weight'])* aloss
+                loss = loss/ self.config['accumulate_batches']
+
+            self.scaler.scale(loss).backward()
+
+            if  (batch_idx + 1)%self.config['accumulate_batches']==0 or (batch_idx + 1) == len(self.iterators['train']):     
+                if self.config['clip'] > 0: # gradient clipping
+                    self.scaler.unscale_(self.optimizer)
+                    nn.utils.clip_grad_norm_(self.model.parameters(), self.config['clip'])
+                self.scaler.step(self.optimizer)   # update
+                self.scaler.update()
+                self.scheduler.step()  ## questionable, may need scaler
+                self.model.zero_grad()  
+
+            tracker['samples'] += [batch['indxs'].cpu().data.numpy()] 
+            tracker['events'] += [(esigmoid.cpu().data.numpy(), batch['elabels'].cpu().data.numpy())]
+            tracker['actions'] += [(asigmoid.cpu().data.numpy(), batch['alabels'].cpu().data.numpy())]
+            tracker['total'] += [loss.item()]
+
+
+            if batch_idx % self.config['log_interval'] == 0:
+                print('Step {:<6}    LOSS = {:10.4f}'.format(step, loss.item()))
+
+        t2 = time()
+        return tracker, t2-t1
+
+    def eval_epoch(self, iter_name='dev', final=False):
+        """
+        Evaluate the model on the test set.
+        No backward computation is allowed.
+        """
+        t1 = time()
+        self.model = self.model.eval()
+        tracker = self.init_tracker()
+
+        with torch.no_grad():
+            for batch_idx, batch in enumerate(self.iterators[iter_name]):
+                tracker['total_samples'] += len(batch['names'])
+
+                for keys in batch.keys():
+                    if keys not in ['names', 'old_pos']:
+                        batch[keys] = batch[keys].to(self.device)
+                
+                with autocast():
+                    eloss, aloss, esigmoid, asigmoid  = self.model(batch)  # forward pass
+                    loss = self.config['event_weight'] * eloss + \
+                        (1 - self.config['event_weight'])* aloss
+                    loss = loss/ self.config['accumulate_batches']
+
+                # collect sigmoid & losses
+                tracker['samples'] += [batch['indxs'].cpu().data.numpy()] 
+                tracker['events'] += [(esigmoid.cpu().data.numpy(), batch['elabels'].cpu().data.numpy())]
+                tracker['actions'] += [(asigmoid.cpu().data.numpy(), batch['alabels'].cpu().data.numpy())]
+                tracker['total'] += [loss.item()]
+
+        t2 = time()
+        return tracker, t2-t1
+
