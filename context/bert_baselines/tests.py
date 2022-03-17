fe785bea6c5615c3a1f22aefdd06af1a0591927a
Jake Vasilakes <jvasilakes@gmail.com>
2022-03-17 15:45:33 +0000

    Update tests

--- context/bert_baselines/tests.py
+++ context/bert_baselines/tests.py
@@ -1,13 +1,14 @@
 import argparse
 import colorama
+import warnings
 
 import torch
 import pytorch_lightning as pl
 
 from config import ExperimentConfig
 from data import n2c2ContextDataModule
-from model import BertMultiHeadedSequenceClassifier
-from layers import TokenMask, EntityPooler
+from models import BertMultiHeadedSequenceClassifier
+from models.layers import TokenMask, EntityPooler
 
 
 colorama.init(autoreset=True)
@@ -26,47 +27,55 @@ def test_logger(func):
             func(*args, **kwargs)
             res_str = "Passed"
             color = colorama.Fore.GREEN
-        except AssertionError:
+        except AssertionError as e:
             res_str = "Failed"
             color = colorama.Fore.RED
+            print(e)
         print(color + f" [{res_str}]")
     return wrapper
 
 
 def run(config_file):
-    config = ExperimentConfig.from_yaml_file(config_file)
+    tmp_config = ExperimentConfig.from_yaml_file(config_file)
+    # Generate default config and override options
+    config = ExperimentConfig()
+    config.data_dir = tmp_config.data_dir
+    config.sentences_dir = tmp_config.sentences_dir
+    config.model_name = "bert-sequence-classifier"
+    config.bert_model_name_or_path = "bert-base-uncased"
+    config.max_seq_length = 300
+    del tmp_config
     pl.seed_everything(config.random_seed)
+    print(config)
 
-    datamodule = n2c2ContextDataModule(
-        config.data_dir,
-        config.sentences_dir,
-        batch_size=config.batch_size,
-        bert_model_name_or_path=config.bert_model_name_or_path,
-        tasks_to_load=config.tasks_to_load,
-        max_seq_length=config.max_seq_length,
-        window_size=config.window_size,
-        max_train_examples=None)
-    datamodule.setup()
-
-    test_mask_hidden(config, datamodule)
-    test_mask_hidden_marked(config, datamodule)
-    test_pool_entity_embeddings_max(config, datamodule)
-    test_pool_entity_embeddings_mean(config, datamodule)
-    test_pool_entity_embeddings_first(config, datamodule)
-    test_pool_entity_embeddings_last(config, datamodule)
-    test_pool_entity_embeddings_first_last(config, datamodule)
+    test_mask_hidden(config)
+    test_mask_hidden_marked(config)
+    test_pool_entity_embeddings_max(config)
+    test_pool_entity_embeddings_mean(config)
+    test_pool_entity_embeddings_first(config)
+    test_pool_entity_embeddings_last(config)
+    test_pool_entity_embeddings_first_last(config)
 
 
 @test_logger
-def test_mask_hidden(config, datamodule):
-    model = BertMultiHeadedSequenceClassifier(
-        config.bert_model_name_or_path,
-        label_spec=datamodule.label_spec,
-        freeze_pretrained=True,
-        use_entity_spans=True,
-        entity_pool_fn="max",
-        lr=config.lr,
-        weight_decay=config.weight_decay)
+def test_mask_hidden(config):
+    data_kwargs = {
+        "mark_entities": False
+    }
+    datamodule = n2c2ContextDataModule.from_config(config, **data_kwargs)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        # Ignore "No test set found"
+        datamodule.setup()
+    assert datamodule.mark_entities is False
+    model_kwargs = {
+        "use_entity_spans": True,
+        "entity_pool_fn": "first",
+    }
+    model = BertMultiHeadedSequenceClassifier.from_config(
+        config, datamodule, **model_kwargs)
+    assert model.use_entity_spans is True
+    assert model.entity_pool_fn == "first"
 
     masker = TokenMask()
     for dl in [datamodule.train_dataloader(), datamodule.val_dataloader()]:
@@ -84,29 +93,30 @@ def test_mask_hidden(config, datamodule):
 
 
 @test_logger
-def test_mask_hidden_marked(config, datamodule):
-    datamodule = n2c2ContextDataModule(
-        config.data_dir,
-        config.sentences_dir,
-        batch_size=1,
-        bert_model_name_or_path=config.bert_model_name_or_path,
-        tasks_to_load=config.tasks_to_load,
-        max_seq_length=config.max_seq_length,
-        window_size=config.window_size,
-        max_train_examples=None,
-        mark_entities=True)
-    datamodule.setup()
+def test_mask_hidden_marked(config):
+    data_kwargs = {
+        "mark_entities": True,
+    }
+    datamodule = n2c2ContextDataModule.from_config(config, **data_kwargs)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        # Ignore "No test set found"
+        datamodule.setup()
+    assert datamodule.mark_entities is True
 
-    model = BertMultiHeadedSequenceClassifier(
-        config.bert_model_name_or_path,
-        label_spec=datamodule.label_spec,
-        freeze_pretrained=True,
-        use_entity_spans=True,
-        entity_pool_fn="max",
-        lr=config.lr,
-        weight_decay=config.weight_decay)
+    model_kwargs = {
+        "use_entity_spans": True,
+        "entity_pool_fn": "first-last",
+    }
+    model = BertMultiHeadedSequenceClassifier.from_config(
+        config, datamodule, **model_kwargs)
+    assert model.use_entity_spans is True
+    assert model.entity_pool_fn == "first-last"
 
-    at_id = datamodule.tokenizer.convert_tokens_to_ids(['@'])[0]
+    start_marker = datamodule.train.START_ENTITY_MARKER
+    end_marker = datamodule.train.END_ENTITY_MARKER
+    start_id = datamodule.tokenizer.convert_tokens_to_ids([start_marker])[0]
+    end_id = datamodule.tokenizer.convert_tokens_to_ids([end_marker])[0]
     masker = TokenMask()
     for dl in [datamodule.train_dataloader(), datamodule.val_dataloader()]:
         for batch in dl:
@@ -121,19 +131,29 @@ def test_mask_hidden_marked(config, datamodule):
             except ValueError:
                 raise AssertionError("Entity span not found.")
             masked_ids = batch["encodings"]["input_ids"][token_mask[:, :, 0]]
-            assert masked_ids[0] == at_id and masked_ids[-1] == at_id
+            assert masked_ids[0] == start_id and masked_ids[-1] == end_id
 
 
 @test_logger
-def test_pool_entity_embeddings_max(config, datamodule):
-    model = BertMultiHeadedSequenceClassifier(
-        config.bert_model_name_or_path,
-        label_spec=datamodule.label_spec,
-        freeze_pretrained=True,
-        use_entity_spans=True,
-        entity_pool_fn="max",
-        lr=config.lr,
-        weight_decay=config.weight_decay)
+def test_pool_entity_embeddings_max(config):
+    data_kwargs = {
+        "mark_entities": False,
+    }
+    datamodule = n2c2ContextDataModule.from_config(config, **data_kwargs)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        # Ignore "No test set found"
+        datamodule.setup()
+    assert datamodule.mark_entities is False
+
+    model_kwargs = {
+        "use_entity_spans": True,
+        "entity_pool_fn": "max",
+    }
+    model = BertMultiHeadedSequenceClassifier.from_config(
+        config, datamodule, **model_kwargs)
+    assert model.use_entity_spans is True
+    assert model.entity_pool_fn == "max"
 
     masker = TokenMask()
     insize = outsize = model.bert_config.hidden_size
@@ -150,15 +170,25 @@ def test_pool_entity_embeddings_max(config, datamodule):
 
 
 @test_logger
-def test_pool_entity_embeddings_mean(config, datamodule):
-    model = BertMultiHeadedSequenceClassifier(
-        config.bert_model_name_or_path,
-        label_spec=datamodule.label_spec,
-        freeze_pretrained=True,
-        use_entity_spans=True,
-        entity_pool_fn="mean",
-        lr=config.lr,
-        weight_decay=config.weight_decay)
+def test_pool_entity_embeddings_mean(config):
+    data_kwargs = {
+        "mark_entities": False,
+    }
+    datamodule = n2c2ContextDataModule.from_config(config, **data_kwargs)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        # Ignore "No test set found"
+        datamodule.setup()
+    assert datamodule.mark_entities is False
+
+    model_kwargs = {
+        "use_entity_spans": True,
+        "entity_pool_fn": "mean",
+    }
+    model = BertMultiHeadedSequenceClassifier.from_config(
+        config, datamodule, **model_kwargs)
+    assert model.use_entity_spans is True
+    assert model.entity_pool_fn == "mean"
 
     masker = TokenMask()
     insize = outsize = model.bert_config.hidden_size
@@ -176,15 +206,25 @@ def test_pool_entity_embeddings_mean(config, datamodule):
 
 
 @test_logger
-def test_pool_entity_embeddings_first(config, datamodule):
-    model = BertMultiHeadedSequenceClassifier(
-        config.bert_model_name_or_path,
-        label_spec=datamodule.label_spec,
-        freeze_pretrained=True,
-        use_entity_spans=True,
-        entity_pool_fn="first",
-        lr=config.lr,
-        weight_decay=config.weight_decay)
+def test_pool_entity_embeddings_first(config):
+    data_kwargs = {
+        "mark_entities": False,
+    }
+    datamodule = n2c2ContextDataModule.from_config(config, **data_kwargs)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        # Ignore "No test set found"
+        datamodule.setup()
+    assert datamodule.mark_entities is False
+
+    model_kwargs = {
+        "use_entity_spans": True,
+        "entity_pool_fn": "first",
+    }
+    model = BertMultiHeadedSequenceClassifier.from_config(
+        config, datamodule, **model_kwargs)
+    assert model.use_entity_spans is True
+    assert model.entity_pool_fn == "first"
 
     masker = TokenMask()
     insize = outsize = model.bert_config.hidden_size
@@ -204,15 +244,25 @@ def test_pool_entity_embeddings_first(config, datamodule):
 
 
 @test_logger
-def test_pool_entity_embeddings_last(config, datamodule):
-    model = BertMultiHeadedSequenceClassifier(
-        config.bert_model_name_or_path,
-        label_spec=datamodule.label_spec,
-        freeze_pretrained=True,
-        use_entity_spans=True,
-        entity_pool_fn="last",
-        lr=config.lr,
-        weight_decay=config.weight_decay)
+def test_pool_entity_embeddings_last(config):
+    data_kwargs = {
+        "mark_entities": False,
+    }
+    datamodule = n2c2ContextDataModule.from_config(config, **data_kwargs)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        # Ignore "No test set found"
+        datamodule.setup()
+    assert datamodule.mark_entities is False
+
+    model_kwargs = {
+        "use_entity_spans": True,
+        "entity_pool_fn": "last",
+    }
+    model = BertMultiHeadedSequenceClassifier.from_config(
+        config, datamodule, **model_kwargs)
+    assert model.use_entity_spans is True
+    assert model.entity_pool_fn == "last"
 
     masker = TokenMask()
     insize = outsize = model.bert_config.hidden_size
@@ -232,15 +282,25 @@ def test_pool_entity_embeddings_last(config, datamodule):
 
 
 @test_logger
-def test_pool_entity_embeddings_first_last(config, datamodule):
-    model = BertMultiHeadedSequenceClassifier(
-        config.bert_model_name_or_path,
-        label_spec=datamodule.label_spec,
-        freeze_pretrained=True,
-        use_entity_spans=True,
-        entity_pool_fn="first-last",
-        lr=config.lr,
-        weight_decay=config.weight_decay)
+def test_pool_entity_embeddings_first_last(config):
+    data_kwargs = {
+        "mark_entities": False,
+    }
+    datamodule = n2c2ContextDataModule.from_config(config, **data_kwargs)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        # Ignore "No test set found"
+        datamodule.setup()
+    assert datamodule.mark_entities is False
+
+    model_kwargs = {
+        "use_entity_spans": True,
+        "entity_pool_fn": "first-last",
+    }
+    model = BertMultiHeadedSequenceClassifier.from_config(
+        config, datamodule, **model_kwargs)
+    assert model.use_entity_spans is True
+    assert model.entity_pool_fn == "first-last"
 
     masker = TokenMask()
     insize = outsize = model.bert_config.hidden_size
