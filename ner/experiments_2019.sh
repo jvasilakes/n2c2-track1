cf91ebd31ed1a62e70ce13b342172703654cc131
Nhung <stampit12@gmail.com>
2023-04-13 13:35:30 +0000

    Clean some data

--- ner/experiments_2019.sh
+++
@@ -1,8 +0,0 @@
-for i in {0..4}
-do  
-    mkdir $i
-    # mkdir $i/baseline+i2b2_2019
-    # mkdir $i/baseline_roberta+i2b2_2019
-    # mkdir $i/clinical_bert+i2b2_2019
-    mv ../experiments/$i/*i2b2_2019 $i    
-done
